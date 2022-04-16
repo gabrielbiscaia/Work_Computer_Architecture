772fa32c65d726d3aeaa8d64eeeca59d5dc9ac9e
Gabriel Biscaia <ra118928@uem.br>
2022-04-15 23:52:32 -0300

    deixando visual

--- registradores/Uc.java
+++ registradores/Uc.java
@@ -12,5 +12,6 @@ public class Uc { // Unidade de controle
     this.opcode = palavras.get(numInstrucao).substring(0, 4);
     this.op1 = palavras.get(numInstrucao).substring(4, 10);
     this.op2 = palavras.get(numInstrucao).substring(10, 16);
+    System.out.println("OPCODE: "+this.opcode+" Operando 1: "+this.op1+" Operando 2: "+this.op2);
   }
 }
