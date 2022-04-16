28e119198d2775287f50f30ba91c94d4142382e4
Gabriel Biscaia <ra118928@uem.br>
2022-04-15 23:53:50 -0300

    diminuindo tamanho da main e conectando tudo

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-import registradores.*;
 public class Main {
     public static void main(String[] args) {
         Cpu cpu = new Cpu();
-        Uc uc = new Uc();
-        Ula ula = new Ula();
-        // cpu.lerPalavra();
-        // System.out.println(cpu.converteOperando("111100"));
-        uc.decode(cpu.lerMemoria());
+        cpu.cicloDeBuscaExecucao();
         // System.out.println(ula.realizaOperac("111111","000010","0001"));
         // System.out.println(ula.realizaOperac("000100","000011","0010"));
         // PSystem.out.println(ula.realizaOperac("000100","001110","0011"));    }
