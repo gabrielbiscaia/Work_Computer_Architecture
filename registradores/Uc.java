d13c4a6b8bca7debc5ff806002330f5ac918c031
Gabriel Biscaia <ra118928@uem.br>
2022-04-15 23:45:48 -0300

    lendo uma palavra por vez

--- registradores/Uc.java
+++ registradores/Uc.java
@@ -4,16 +4,13 @@ import java.util.ArrayList;
 
 public class Uc { // Unidade de controle
   public String instrucao;
-  public ArrayList<String> opcode = new ArrayList<String>();
-  public ArrayList<String> op1 = new ArrayList<String>();
-  public ArrayList<String> op2 = new ArrayList<String>();
+  public String opcode;
+  public String op1;
+  public String op2;
 
-  public void decode(ArrayList<String> palavras) {
-    for (int i = 0; i < palavras.size(); i++) {
-      this.opcode.add(palavras.get(i).substring(0, 4));
-      this.op1.add(palavras.get(i).substring(4, 10));
-      this.op2.add(palavras.get(i).substring(10, 16));
-      System.out.println(this.op2);
-    }
+  public void decode(ArrayList<String> palavras, int numInstrucao) {
+    this.opcode = palavras.get(numInstrucao).substring(0, 4);
+    this.op1 = palavras.get(numInstrucao).substring(4, 10);
+    this.op2 = palavras.get(numInstrucao).substring(10, 16);
   }
 }
