13375172328d1e0fbf0d34f56879db8e513012cd
Gabriel Biscaia <ra118928@uem.br>
2022-04-17 16:51:59 -0300

    comentario na função

--- registradores/Uc.java
+++ registradores/Uc.java
@@ -7,7 +7,7 @@ public class Uc { // Unidade de controle
   public String op1;
   public String op2;
 
-  public void decode(ArrayList<String> palavras, int numInstrucao) {
+  public void decode(ArrayList<String> palavras, int numInstrucao) {//função para separar os operandos
     this.opcode = palavras.get(numInstrucao).substring(0, 4);
     this.op1 = palavras.get(numInstrucao).substring(4, 10);
     this.op2 = palavras.get(numInstrucao).substring(10, 16);
